354319c8c2af4b5ef80490235b51fbdc04f16489
Bekhruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-05-19 08:44:49 +0500

    Adding commits

--- app/api/routes/predictor.py
+++ app/api/routes/predictor.py
@@ -13,13 +13,20 @@ router = APIRouter()
 @router.post("/detect", response_model=Detection, name="image:get-data")
 async def image(file: UploadFile = File(...)):
     try:
+        # Load image
         contents = await file.read()
-        nparr = np.fromstring(contents, np.uint8)
-        frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        print(frame.shape)
 
+        # Decode image to numpy array
+        nparr = np.fromstring(contents, np.uint8)
+
+        # Convert numpy array to image
+        frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        # Detect eye blinks
         result, pred_l, pred_r = eye_blink_detection(frame)
 
+        # Return result
+        # TODO: you can return more than one result
         if result is not None:
             if result == 'Blink':
                 return Detection(status=False)
@@ -29,6 +36,6 @@ async def image(file: UploadFile = File(...)):
         else:
             return Detection(status=False)
 
-
+    # Catch errors
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Exception: {e}")
