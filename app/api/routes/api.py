5b64e473ff0b4dd0e8721c65b5a3ff43cf0925f8
Bekhruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-05-19 08:25:52 +0500

    API tags and prefix

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -2,5 +2,6 @@ from fastapi import APIRouter
 
 from api.routes import predictor
 
+# API TAGS and PREFIX
 router = APIRouter()
 router.include_router(predictor.router, tags=["predictor"], prefix="/v1")
